4dbe476d571ee35129a6075f82533d3c3243ee45
Xvezda <xvezda@naver.com>
2020-06-02 03:47:55 +0900

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  mode: process.NODE_ENV || 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.s?css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'sass-loader'
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/index.html', to: 'index.html' }
+      ]
+    }),
+    new ExtractTextPlugin('bundle.css'),
+  ],
+  resolve: {
+    alias: {
+      "react": "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+    }
+  }
+}
